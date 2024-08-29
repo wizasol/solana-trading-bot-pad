47840b8ac02155f16f694070fc3070937f3d39ce
solkeen <nakao95911@gmail.com>
2024-08-30 03:40:11 +0800

    change: x.om

--- src/pages/readme/index.tsx
+++ src/pages/readme/index.tsx
@@ -9,7 +9,7 @@ const Welcome = () => {
             <p style={{marginBottom : '30px' , marginTop : '30px'}}>If you wanna get perfect bot , Contact me</p>
             <p>Discord : <a href="https://discordapp.com/users/471524111512764447">! solkeen !</a></p>
             <p>Telegram : <a href="https://t.me/so1keen">@solkeen</a></p>
-            <p>Telegram : <a href="https://x.com/so1keen">@solkeen</a></p>
+            <p>X.com : <a href="https://x.com/so1keen">@solkeen</a></p>
         </>
     )
 }
