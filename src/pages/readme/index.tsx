c90cfb650b644eefbfd7bb262dd12421b8813b07
solkeen <nakao95911@gmail.com>
2024-08-30 03:31:12 +0800

    change: Contact Info commit by solkeen

--- src/pages/readme/index.tsx
+++ src/pages/readme/index.tsx
@@ -3,6 +3,13 @@ const Welcome = () => {
     return (
         <>
             <h1>Welcome to BotPad</h1>
+            <p>BotPad is Solkeen's Trading Bot consists of Sniping bot , Copytrading Bot , Volume Bot and so on.</p>
+            <p>This bot runs on several Solana dex like Pump.fun , Raydium , etc ..</p>
+            <p>This Bot is beta version and missing port will be coming soon.</p>
+            <p style={{marginBottom : '30px' , marginTop : '30px'}}>If you wanna get perfect bot , Contact me</p>
+            <p>Discord : <a href="https://discordapp.com/users/471524111512764447">! solkeen !</a></p>
+            <p>Telegram : <a href="https://t.me/so1keen">@solkeen</a></p>
+            <p>Telegram : <a href="https://x.com/so1keen">@solkeen</a></p>
         </>
     )
 }
